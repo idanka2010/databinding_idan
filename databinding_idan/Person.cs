0f031c453a578225adb4bb752c198fbd11bdeb49
עידן פרולוב <idan.frolov@office.eduil.org>
2024-12-30 21:04:43 +0200

    Add project files.

---
+++ databinding_idan/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace databinding_idan
+{
+    public class Person
+    {
+        private string email;
+        private string password;
+
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+
+        public string Password
+        {
+            get => password;
+            set => password = value;
+        }
+    }
+}
